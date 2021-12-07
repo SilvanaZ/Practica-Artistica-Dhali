acfc15fe4bb33fa3e86d15a51fa6d5015b32a749
Silvana Zalazar <silvana.blaq@gmail.com>
2021-12-07 18:50:09 -0300

    modify

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,10 @@ let express = require('express')
 let router = express.Router()
 let controller = require('../controllers/productsController')
 
-router.get('/detail', controller.detail)
+/* GET - Product Detail */
+router.get('/detail/:id', controller.detail)
+
+/* GET - List of products */
+router.get('/category/:id', controller.category)
 
 module.exports = router
