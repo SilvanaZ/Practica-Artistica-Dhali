13309321e833d47ddd4a8b61f17147d8c3bfc3d6
Silvana Zalazar <silvana.blaq@gmail.com>
2021-12-10 17:00:55 -0300

    adding changes on visual

--- src/app.js
+++ src/app.js
@@ -2,23 +2,28 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000
+const methodOverride = require('method-override')
 
 /* Middlewares */
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs") // Setea el template engine
 app.set('views', path.join(__dirname, 'views')) // Indica la ubicación de la carpeta views 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 /* Enrutadores */
 let indexRouter = require('./routes/index')
 let productsRouter = require('./routes/products')
-let usersRouter = require('/routes/admin')
+let usersRouter = require('./routes/users')
+let adminRouter = require('./routes/admin')
 
 /* Routes */
 app.use('/', indexRouter)
 app.use('/products', productsRouter)
-
-
+app.use('/users', usersRouter)
+app.use('/admin', adminRouter)
 
 
 app.listen(PORT, () => console.log(`
--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -10,7 +10,48 @@ let controller = {
     create: (req, res)=>{
         res.render('/admin/adminProductCreateForm')
     },
+    store: (req, res) => {
+        let lastId = 1;
+
+        products.forEach(product => {
+            if(product.id > lastId){
+                lastId = product.id
+            }
+        });
+
+        const {name, price, category, subcategory, description, discount} = req.body
+
+        let newProduct = {
+            name: name.trim(),
+            price: +price.trim(),
+            category: +category,
+            subcategory,
+            description: description.trim(),
+            discount: +discount,
+            image: req.file ? req.file.filename : "default-image.png"
+        }
+
+        /* let newProduct = {
+            ...req.body,
+            id: lastId + 1,
+            image: req.file ? req.file.filename : "default-image.png"
+        } */
+
+        products.push(newProduct)
+
+        writeProductsJSON(products)
+
+        req.redirect('/admin/products')
+    },
     edit: (req, res)=>{
-        res.render('/admin/adminProductEditForm)
+        res.render('/admin/adminProductEditForm')
+    },
+    update: (req, res) =>{
+
+    },
+    destroy: (req, res)=>{
+
     }
 }
+
+module.exports = controller
